4f7b70661da17d0628a85c267742e9db06652159
Ivan <3785256@gmail.com>
2022-12-21 17:19:14 +0300

    added notificationRest

--- src/App.js
+++ src/App.js
@@ -4,33 +4,20 @@ import InputText from './components/InputText';
 import Keyboard from './components/Keyboard';
 import { AutoContext } from './context';
 import "./styles/App.css";
+import { arrayWords } from './wordsToPrint'
 
-const arrayWords = ["блог",
-        "бинокль",
-        "Россия",
-        "агрегат",
-        "экономика",
-        "работа",
-        "инвестиции",
-        "сбережения",
-        "стул",
-        "стол",
-        "гравий",
-        "локация",
-        "финансы",
-        "кредит",
-        "теория",
-        "практика",
-        "наводнение",
-        "фикция",
-        "дрон",
-        "шатёр",
-        "щавель",
-        "улей",
-        "Евпатория",
-        "Бангладеш",
-        "Беларусь",
-        "характер"];
+const selectWord = (arrLength) => {
+  let randomNum = Math.floor(arrLength * Math.random());
+  return arrayWords[randomNum];
+}
+
+const generateText = (word) => {
+    let string = '';
+    for (let i = 0; i < 70; i += word.length + 1) {
+        string = string + word + ' ';
+    }
+    return string;
+}
 
 function App() {
   const [time, setTime] = useState(5);
@@ -45,65 +32,40 @@ function App() {
   const [lastLetter, setLastLetter] = useState(randomText[0]);
   const [textLength, setTextLength] = useState(randomText.length);
   const [objectLetter, setObjectLetter] = useState();
-  const [shiftButton, setShiftButton] = useState();
+  const [shiftButton, setShiftButton] = useState(); 
 
-  function selectWord(arrLength) {
-    let randomNum = Math.floor(arrLength * Math.random());
-    return arrayWords[randomNum];
+  function resetText(){
+    setText('');
+    let sentence = generateText(selectWord(arrayWords.length)); 
+    setLastLetter(sentence[0]);  
+    setRandomText(sentence);
   }
-
-
-function generateText(word) {
-    let string = '';
-    for (let i = 0; i < 70; i += word.length + 1) {
-        string = string + word + ' ';
-    }
-
-    return string;
-} 
-
-function resetText(){
-  setText('');
-  let sentence = generateText(selectWord(arrayWords.length)); 
-  setLastLetter(sentence[0]);  
-  setRandomText(sentence);
-}
 
   useEffect(() => {
     if(isRestart){
-      resetText();
-      setIsRestart(false);
+      resetText()
+      if(currentTime === 0){
+        setCurrentTime(time*60)
+      }
+      setIsRestart(false)
     }
   },[isRestart]) 
 
   return (
     <AutoContext.Provider value={{
-      randomText,
-      currentText,
-      setText,
-      setRandomText,
-      shiftButton, 
-      setShiftButton,
-      time, 
-      setTime,
-      objectLetter, 
-      setObjectLetter,
-      currentTime,
-      setCurrentTime,
-      isFinished, 
-      setIsFinished,
-      isStarted, 
-      setIsStarted,
-      textLength, 
-      setTextLength,
-      lastLetter, 
-      setLastLetter,
-      errors, 
-      setErrors,
-      isRestart, 
-      setIsRestart,
-      timeWrite, 
-      setTimeWrite,
+      randomText, setRandomText,
+      currentText, setText, 
+      shiftButton, setShiftButton,
+      time, setTime,
+      objectLetter, setObjectLetter,
+      currentTime,setCurrentTime,
+      isFinished, setIsFinished,
+      isStarted, setIsStarted,
+      textLength, setTextLength,
+      lastLetter, setLastLetter,
+      errors, setErrors,
+      isRestart, setIsRestart,
+      timeWrite, setTimeWrite
     }}>
       <div className="App">
         <Header/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,46 +9,43 @@ const Header = () => {
     const {isFinished, textLength, errors, timeWrite, setIsRestart} = useContext(AutoContext);
     const [currentErrors, setCurrentErrors] = useState('--')
     const [currentSpeed, setCurrentSpeed] = useState('--')
-    let isComplete = false;
 
     useEffect(() => {
         if(isFinished === true){
             setCurrentErrors(String(errors))
         }
 
-        if(isFinished === true && isComplete === false){
+        if(isFinished === true){
             setCurrentSpeed(Math.floor(textLength/(timeWrite/60000)));
-            isComplete = true;
         }
-
-    }, [isFinished, isComplete])
+    }, [isFinished])
 
     return(
         <header>
             <div className="wrapper">
                 <div className="header__container">
-                <div className="header__left">
-                <a href="#">
-                   <img src="logo.png" alt="logo" />
-                </a>
-                <button className="text_reset"
-                        onClick={() => {
-                            setIsRestart(true);
-                        }}>
-                    <img src="restart.png" alt="" width='20px'/>
-                </button>
-            </div>
+                    <div className="header__left">
+                        <a href="#">
+                            <img src="logo.png" alt="logo" />
+                        </a>
+                        <button className="text_reset"
+                            onClick={() => {
+                                setIsRestart(true);
+                            }}>
+                            <img src="restart.png" alt="" width='20px'/>
+                        </button>
+                    </div>
             <Timer/>
-            <div className="header__right">
-                <div className="speed">
-                    <img src="speed.png" alt="sp" width='20'/>
-                    <p title="Скорость печати, симв/мин">{currentSpeed}</p>
-                </div>
-                <div className="mistakes">
-                    <img src="stop.png" alt="" width='20'/>
-                    <p title="Число ошибок">{currentErrors}</p>
-                </div>
-            </div>
+                    <div className="header__right">
+                        <div className="speed">
+                            <img src="speed.png" alt="sp" width='20'/>
+                            <p title="Скорость печати, симв/мин">{currentSpeed}</p>
+                        </div>
+                        <div className="mistakes">
+                            <img src="stop.png" alt="" width='20'/>
+                            <p title="Число ошибок">{currentErrors}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
